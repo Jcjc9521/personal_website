28852fc6ee2f99d42cce116f6328bd957a36956b
Jcjc9521 <60627344+Jcjc9521@users.noreply.github.com>
2025-05-14 20:39:48 +0000

    Deploying to gh-pages from @ Jcjc9521/personal_website@ff9bf13790f804b005ad71eecc1e96bd994b6282 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-about",
-    title: "about",
+    id: "nav-zhiheng-wang-research-group",
+    title: "Zhiheng Wang Research Group",
     section: "Navigation",
     handler: () => {
       window.location.href = "/personal_website/";
